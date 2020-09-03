6985b8208fe30c6dc18ce9459f68fed41268d6ce
adamdrira <59208754+adamdrira@users.noreply.github.com>
2020-09-03 13:38:11 +0200

    Add files via upload

--- src/app/helpers/dates.ts
+++ src/app/helpers/dates.ts
@@ -52,12 +52,65 @@ export function get_date_to_show(s: number) {
 
 }
 
+export function get_date_to_show_chat(s: number) {
+    
+  if( s < 3600 ) {
+      if( Math.trunc(s/60)==1 ) {
+        return "1 min";
+      }
+      else {
+        return Math.trunc(s/60) + " min";
+      }
+    }
+    else if( s < 86400 ) {
+      if( Math.trunc(s/3600)==1 ) {
+        return "1 h";
+      }
+      else {
+        return Math.trunc(s/3600) + " h";
+      }
+    }
+    else if( s < 604800 ) {
+      if( Math.trunc(s/86400)==1 ) {
+        return "1 j";
+      }
+      else {
+        return Math.trunc(s/86400) + " j";
+      }
+    }
+    else if ( s < 2419200 ) {
+      if( Math.trunc(s/604800)==1 ) {
+        return "1 sem";
+      }
+      else {
+        return  Math.trunc(s/604800) + " sem";
+      }
+    }
+    else if ( s < 9676800 ) {
+      if( Math.trunc(s/2419200)==1 ) {
+        return "1 mois";
+      }
+      else {
+        return  Math.trunc(s/2419200) + " mois";
+      }
+    }
+    else {
+      if( Math.trunc(s/9676800)==1 ) {
+        return "1 an";
+      }
+      else {
+        return Math.trunc(s/9676800) + " ans";
+      }
+    }
+
+}
+
 export function date_in_seconds(now: number, date_upload:string){
 
     var uploaded_date = date_upload.substring(0,date_upload.length - 5);
     uploaded_date = uploaded_date.replace("T",' ');
     uploaded_date = uploaded_date.replace("-",'/').replace("-",'/');
-    const uploaded_date_in_second = new Date(uploaded_date + ' GMT').getTime()/1000;
+    const uploaded_date_in_second = Math.trunc(new Date(uploaded_date + ' GMT').getTime()/1000);
 
     return ( now - uploaded_date_in_second );
 }
