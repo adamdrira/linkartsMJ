6ee5504e159a9aa27dd83cd06f9f2ce6c67af14f
adamdrira <59208754+adamdrira@users.noreply.github.com>
2020-09-08 15:11:03 +0200

    Add files via upload

---
+++ src/app/popup-login/popup-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popup-login',
+  templateUrl: './popup-login.component.html',
+  styleUrls: ['./popup-login.component.scss']
+})
+export class PopupLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
