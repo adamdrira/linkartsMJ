cc2a3bbf9ceae29ddf0640f10dfb975620688ced
adamdrira <59208754+adamdrira@users.noreply.github.com>
2020-11-17 00:29:17 +0100

    Add files via upload

--- src/app/add-comics-chapter/add-comics-chapter.component.ts
+++ src/app/add-comics-chapter/add-comics-chapter.component.ts
@@ -29,43 +29,52 @@ export class AddComicsChapterComponent implements OnInit {
   list_of_chapters_retrieved=false;
   comics_data:any;
   comics_data_retrieved=false;
-  
+  user_id:number;
   profile_picture:SafeUrl;
   pseudo:string;
+  data_retrieved=false;
+
+
 
   ngOnInit(): void {
 
     this.bd_id = parseInt(this.activatedRoute.snapshot.paramMap.get('id'));
     console.log(this.bd_id)
 
-
-
+    this.BdSerieService.retrieve_bd_by_id(this.bd_id).subscribe(r=>{
+      console.log(r)
+      this.comics_data=r[0];
+      this.comics_data_retrieved=true;
+      this.user_id=r[0].authorid;
+      this.Profile_Edition_Service.retrieve_profile_data(r[0].authorid).subscribe(m=>{
+        this.pseudo=m[0].nickname;
+       
+        this.Profile_Edition_Service.retrieve_profile_picture( this.user_id ).subscribe(r=> {
+          let url = (window.URL) ? window.URL.createObjectURL(r) : (window as any).webkitURL.createObjectURL(r);
+          const SafeURL = this.sanitizer.bypassSecurityTrustUrl(url);
+          this.profile_picture = SafeURL;
+          this.data_retrieved=true;
+        })
+      })
+    })
 
     this.BdSerieService.retrieve_chapters_by_id(this.bd_id).subscribe(r=>{
       console.log(r[0])
       this.list_of_chapters=r[0];
       this.list_of_chapters_retrieved=true;
-      
-
-      /*this.Profile_Edition_Service.retrieve_profile_picture( r[0].authorid ).subscribe(r=> {
-        let url = (window.URL) ? window.URL.createObjectURL(r) : (window as any).webkitURL.createObjectURL(r);
-        const SafeURL = this.sanitizer.bypassSecurityTrustUrl(url);
-        this.profile_picture = SafeURL;
-      });
-      this.Profile_Edition_Service.retrieve_profile_data( r[0].authorid ).subscribe(r=> {
-        this.pseudo=r[0].nickname;
-      });*/
-
-    })
-    
-
-    this.BdSerieService.retrieve_bd_by_id(this.bd_id).subscribe(r=>{
-      console.log(r)
-      this.comics_data=r[0];
-      this.comics_data_retrieved=true;
     })
 
     
+  }
+
+  logo_is_loaded=false;
+  pp_is_loaded=false;
+  load_logo(){
+    this.logo_is_loaded=true;
+  }
+
+  load_pp(){
+    this.pp_is_loaded=true;
   }
 
 }
