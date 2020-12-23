16c25463ee022b2f35200e627df8a5c133069866
adamdrira <59208754+adamdrira@users.noreply.github.com>
2020-12-23 04:34:17 +0100

    Add files via upload

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChange } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange, ChangeDetectorRef } from '@angular/core';
 import {ElementRef, Renderer2, ViewChild, ViewChildren} from '@angular/core';
 import {QueryList} from '@angular/core';
 import { SimpleChanges } from '@angular/core';
@@ -14,7 +14,7 @@ import { Writing_Upload_Service } from '../services/writing.service';
 import { NotificationsService } from '../services/notifications.service';
 import { Profile_Edition_Service } from '../services/profile_edition.service';
 import { Subscribing_service } from '../services/subscribing.service';
-import { ConstantsService } from '../services/constants.service';
+import { Trending_service } from '../services/trending.service';
 import { Favorites_service } from '../services/favorites.service';
 import { ActivatedRoute } from '@angular/router';
 import { trigger, transition, style, animate } from '@angular/animations';
@@ -41,7 +41,7 @@ export class FavoritesComponent implements OnInit {
   constructor(
     private rd: Renderer2,
     public route: ActivatedRoute, 
-    private _constants: ConstantsService,
+    private Trending_service:Trending_service,
     private NotificationsService:NotificationsService,
     private Community_recommendation:Community_recommendation,
     private BdOneShotService:BdOneShotService,
@@ -51,8 +51,8 @@ export class FavoritesComponent implements OnInit {
     private Drawings_Onepage_Service:Drawings_Onepage_Service,
     private Drawings_Artbook_Service:Drawings_Artbook_Service,
     private Writing_Upload_Service:Writing_Upload_Service,
-    private Favorites_service:Favorites_service
-
+    private Favorites_service:Favorites_service,
+    private cd:ChangeDetectorRef,
     ) { }
 
   subcategory: number = 0; 
@@ -62,22 +62,99 @@ export class FavoritesComponent implements OnInit {
   now_in_seconds:number= Math.trunc( new Date().getTime()/1000);
   list_of_users=[];
   favorites_retrieved=false;
+  skeleton:boolean=true;
+
   ngOnInit() {
 
 
     this.Favorites_service.generate_or_get_favorites().subscribe(info=>{
-      console.log(info[0].favorites)
-      console.log(info[0].favorites[0])
-      if(info[0].favorites[0]){
+      console.log(info[0])
+      if(info[0].favorites){
         for(let i=0;i<info[0].favorites.length;i++){
           this.list_of_users[i]=info[0].favorites[i]
           this.list_of_users[i].id= this.list_of_users[i].id_user;
+          let format=(this.list_of_users[i].shares)?'group':'user';
+          this.send_notification(this.list_of_users[i].id, this.list_of_users[i].rank,format)
         }
         console.log(this.list_of_users)
         this.favorites_retrieved=true;
       }
+      else if(info[0].list_of_users){
+        
+        this.list_of_users=info[0].list_of_users;
+        console.log(info[0].list_of_rankings)
+        console.log(this.list_of_users)
+        this.delete_empty_elements(this.list_of_users,info[0].list_of_rankings)
+      }
       
     });
+  }
+
+
+  delete_empty_elements(list,list_of_rankings){
+    console.log(list)
+    console.log(list_of_rankings)
+    let length1=list.length;
+    if(list.length>0){
+      for(let i=0;i<length1;i++){
+        if(!list[length1-1-i]){
+          list.splice(length1-1-i,1)
+        }
+      }
+      
+      let length2=list.length
+      if(length2>=15){
+        for(let j=15;j<length2;j++){
+          list.splice(list.length-1,1)
+        }
+      }
+      this.favorites_retrieved=true;
+      for(let j=0;j<list.length;j++){
+        let format=(list[j].gender=='Groupe')?'group':'user';
+        console.log(list[j])
+        this.send_notification(list[j].id, list_of_rankings[j],format)
+      }
+       
+      
+    }
+    else{
+      return list
+    }
+  }
+
+  send_notification(id,rank,format){
+   
+    console.log(id)
+    this.Trending_service.get_date_of_trendings().subscribe(d=>{
+
+      let date = d[0].date;
+      this.NotificationsService.add_notification_trendings('favorites',1,'Linkarts',id,"favorites","favorites",format,id,rank,date,false,0).subscribe(l=>{
+    
+        if(!l[0].found){
+          let message_to_send ={
+            for_notifications:true,
+            type:"favorites",
+            id_user_name:'Linkarts',
+            id_user:1, // id de linkarts
+            id_receiver:id,
+            publication_category:"favorites",
+            publication_name:"favorites",
+            format:format,
+            publication_id:id,
+            chapter_number:rank,
+            information:date,
+            status:"unchecked",
+            is_comment_answer:false,
+            comment_id:0,
+          }
+          this.ChatService.messages.next(message_to_send);
+         
+        }
+        
+      })
+    })
+
+    
   }
   
 }
