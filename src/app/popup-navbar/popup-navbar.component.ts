eb407f4af17175c177c4a5dc2aeaefab91ca9460
adamdrira <59208754+adamdrira@users.noreply.github.com>
2021-01-30 20:10:57 +0100

    Add files via upload

--- src/app/popup-navbar/popup-navbar.component.ts
+++ src/app/popup-navbar/popup-navbar.component.ts
@@ -248,28 +248,24 @@ export class PopupNavbarComponent implements OnInit {
     }
     this.disconnecting=true;
     
-    this.AuthenticationService.logout();
-    //this.router.navigate(["/"]);
-    let recommendations_string = this.CookieService.get('recommendations');
-    console.log(recommendations_string)
-    if(recommendations_string){
-      this.disconnecting=false;
-      this.location.go('/')
-      location.reload();
-     
-    }
-    else{
-      this.Community_recommendation.generate_recommendations().subscribe(r=>{
+    this.AuthenticationService.logout().subscribe(r=>{
+      let recommendations_string = this.CookieService.get('recommendations');
+      console.log(recommendations_string)
+      if(recommendations_string){
         this.disconnecting=false;
-        this.cd.detectChanges();
         this.location.go('/')
         location.reload();
-      })
-    }
-    
-    /*)*/
-  
-    //this.type_of_profile="visitor";
+       
+      }
+      else{
+        this.Community_recommendation.generate_recommendations().subscribe(r=>{
+          this.disconnecting=false;
+          this.cd.detectChanges();
+          this.location.go('/')
+          location.reload();
+        })
+      }
+    });
     
   }
 
