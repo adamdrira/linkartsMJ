9f626e3399fd02ac335bc5a7cfe654e04c3e062a
adamdrira <59208754+adamdrira@users.noreply.github.com>
2021-02-27 21:30:26 +0100

    Add files via upload

--- src/app/media-see-more-writings/media-see-more-writings.component.ts
+++ src/app/media-see-more-writings/media-see-more-writings.component.ts
@@ -27,7 +27,7 @@ export class MediaSeeMoreWritingsComponent implements OnInit {
   @Input() sorted_artpieces_roman: any[];
   @Input() sorted_artpieces_scenario: any[];
   @Input() sorted_artpieces_article: any[];
-
+  @Input() sorted_artpieces_poetry: any[];
 
   new_sorted_artpieces: any[]=[];
   new_sorted_artpieces_added=false;
@@ -50,8 +50,10 @@ export class MediaSeeMoreWritingsComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log("init see more")
+    console.log(this.style)
     if(this.style=="Article"){
-      this.Community_recommendation.see_more_recommendations_bd("Article").subscribe(r=>{
+      this.Community_recommendation.see_more_recommendations_writings("Article").subscribe(r=>{
         for (let i=0;i<Object.keys(r[0].list_to_send).length;i++){
               let check = JSON.stringify(this.sorted_artpieces_article).includes(JSON.stringify(r[0].list_to_send[i][0]));
               if (!check){
@@ -69,7 +71,7 @@ export class MediaSeeMoreWritingsComponent implements OnInit {
     }
 
     if(this.style=="Scénario"){
-      this.Community_recommendation.see_more_recommendations_bd("Scenario").subscribe(r=>{
+      this.Community_recommendation.see_more_recommendations_writings("Scenario").subscribe(r=>{
         for (let i=0;i<Object.keys(r[0].list_to_send).length;i++){
               let check = JSON.stringify(this.sorted_artpieces_scenario).includes(JSON.stringify(r[0].list_to_send[i][0]));
               if (!check){
@@ -86,8 +88,26 @@ export class MediaSeeMoreWritingsComponent implements OnInit {
       })
     }
 
+    if(this.style=="Poésie"){
+      this.Community_recommendation.see_more_recommendations_writings("Poetry").subscribe(r=>{
+        for (let i=0;i<Object.keys(r[0].list_to_send).length;i++){
+              let check = JSON.stringify(this.sorted_artpieces_poetry).includes(JSON.stringify(r[0].list_to_send[i][0]));
+              if (!check){
+                this.new_sorted_artpieces.push(r[0].list_to_send[i][0]);
+                if(i==Object.keys(r[0].list_to_send).length-1){
+                  this.new_sorted_artpieces_added=true;
+                }
+              }
+              if(check && i==Object.keys(r[0].list_to_send).length-1){
+                this.new_sorted_artpieces_added=true;
+              }
+              
+            }
+      })
+    }
+
     if(this.style=="Roman"){
-      this.Community_recommendation.see_more_recommendations_bd("Roman").subscribe(r=>{
+      this.Community_recommendation.see_more_recommendations_writings("Roman").subscribe(r=>{
         for (let i=0;i<Object.keys(r[0].list_to_send).length;i++){
               let check = JSON.stringify(this.sorted_artpieces_roman).includes(JSON.stringify(r[0].list_to_send[i][0]));
               if (!check){
@@ -105,7 +125,9 @@ export class MediaSeeMoreWritingsComponent implements OnInit {
     }
 
     if(this.style=="Roman illustré"){
-      this.Community_recommendation.see_more_recommendations_bd("illustrated novel").subscribe(r=>{
+      this.Community_recommendation.see_more_recommendations_writings("Illustrated novel").subscribe(r=>{
+        console.log("réponse roman")
+        console.log(r)
         for (let i=0;i<Object.keys(r[0].list_to_send).length;i++){
               let check = JSON.stringify(this.sorted_artpieces_illustrated_novel).includes(JSON.stringify(r[0].list_to_send[i][0]));
               if (!check){
@@ -126,44 +148,6 @@ export class MediaSeeMoreWritingsComponent implements OnInit {
 
 
 
-
- 
-  /*sendLoaded(object){
-    console.log("loaded")
-    this.number_of_loaded++;
-    console.log(this.number_of_thumbnails);
-    if(this.number_of_loaded==this.number_of_thumbnails){
-      console.log("updating_list")
-      this.update_lists(this.number_of_writings_to_show);
-    }
-  }
-
-
-  update_lists(number){
-    if( number== 1 ) {
-      $(".thumbnail-component-container:nth-of-type(1), .thumbnail-component-container:nth-of-type(2), .thumbnail-component-container:nth-of-type(3), .thumbnail-component-container:nth-of-type(4), .thumbnail-component-container:nth-of-type(5), .thumbnail-component-container:nth-of-type(6)").css("display","block");
-    }
-    else if( number== 2) {
-      $(".thumbnail-component-container:nth-of-type(1), .thumbnail-component-container:nth-of-type(2)").css("display","block");
-      $(".thumbnail-component-container:nth-of-type(3), .thumbnail-component-container:nth-of-type(4), .thumbnail-component-container:nth-of-type(5), .thumbnail-component-container:nth-of-type(6)").css("display","none");
-    }
-    else if( number== 3) {
-      $(".thumbnail-component-container:nth-of-type(1), .thumbnail-component-container:nth-of-type(2), .thumbnail-component-container:nth-of-type(3)").css("display","block");
-      $(".thumbnail-component-container:nth-of-type(4), .thumbnail-component-container:nth-of-type(5), .thumbnail-component-container:nth-of-type(6)").css("display","none");
-    }
-    else if( number== 4 ) {
-      $(".thumbnail-component-container:nth-of-type(1), .thumbnail-component-container:nth-of-type(2), .thumbnail-component-container:nth-of-type(3), .thumbnail-component-container:nth-of-type(4)").css("display","block");
-      $(".thumbnail-component-container:nth-of-type(5), .thumbnail-component-container:nth-of-type(6)").css("display","none");
-    }
-    else if( number== 5) {
-      $(".thumbnail-component-container:nth-of-type(1), .thumbnail-component-container:nth-of-type(2), .thumbnail-component-container:nth-of-type(3), .thumbnail-component-container:nth-of-type(4), .thumbnail-component-container:nth-of-type(5)").css("display","block");
-      $(".thumbnail-component-container:nth-of-type(6)").css("display","none");
-    }
-    else if( number== 6) {
-      $(".thumbnail-component-container:nth-of-type(1), .thumbnail-component-container:nth-of-type(2), .thumbnail-component-container:nth-of-type(3), .thumbnail-component-container:nth-of-type(4), .thumbnail-component-container:nth-of-type(5), .thumbnail-component-container:nth-of-type(6)").css("display","block");
-    }
-    this.send_put_more_visible.emit(true)
-  }*/
 
 
 
