56de8509d0b47ebdd8f0364093132216f059bec8
adamdrira <59208754+adamdrira@users.noreply.github.com>
2021-03-08 00:03:10 +0100

    Add files via upload

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -10,10 +10,11 @@ export class ConstantsService {
   constructor() { }
 
 
-  drawings_filters=["Abstrait","Animaux","Animation","Architecture","Art","Botanique","BD","Campagne","Caricatural","Cartoons","Cinéma","Comics","Culture","Décoration","Design","Enigmatique","Enfants","Fanart","Fanfiction","Fantaisie","Femmes","Fête","Fresque","Guerre","Guerrier","Graffiti","Héroïque","Histoire","Hommes","Horreur","Humour","Immeubles","Intérieur","Kawaii","Maisons","Manga","Monstre","Montagnes","Motos","Nature","Nature morte","Paysage","Personnages","Photographie","Portrait",
+  drawings_filters=["Abstrait","Animaux","Animés","Architecture","Art","Botanique","BD","Campagne","Caricatural","Cartoons","Cinéma","Comics","Culture","Décoration","Design","Enigmatique","Enfants","Fanart","Fanfiction","Fantaisie","Femmes","Fête","Fresque","Guerre","Guerrier","Graffiti","Héroïque","Histoire","Hommes","Horreur","Humour","Immeubles","Intérieur","Kawaii","Maisons",
+  "Mangas","Monstre","Montagnes","Motos","Nature","Nature morte","Paysage","Personnages","Photographie","Portrait",
   "Réaliste","Religion","Retro","Romantique","Rural","SF","Sociologie","Spatial","Sport","Survie","Transports","Urbanisme","Véhicules","Villes","Voitures","Voyage","Western"]
-  writings_filters=["Action","Animaux","Art","Aventure","Autobiographie","Caricatural","Enigmatique","Enfants","Epique","Epistolaire","Esotérisme","Fanfiction","Fantaisie","Guerre","Héroïque","Histoire","Horreur","Humour","Journalisme","Pensées","Philosophie","Polar",
-  "Policier","Réaliste","Réflexions","Religion","Romantique","Satirique","SF","Sociologie","Spatial","Spiritualité","Sport","Survie","Thriller","Voyage","Western"];
+  writings_filters=["Action","Animaux","Animés","Art","Aventure","Autobiographie","BD","Caricatural","Cartoons","Comics","Enigmatique","Enfants","Epique","Epistolaire","Esotérisme","Fanfiction","Fantaisie","Guerre","Héroïque","Histoire","Horreur","Humour","Journalisme","Mangas","Pensées","Philosophie","Polar",
+  "Policier","Réaliste","Réflexions","Religion","Romantique","Satirique","SF","Sociologie","Spatial","Spiritualité","Sport","Survie","Thriller","Voyage","ebtoon","Western"];
   comics_filters=["Action","Animaux","Art","Aventure","Biographie","Caricatural","Enigmatique","Enfants","Epique","Esotérisme","Fanfiction","Fantaisie","Fantastique","Guerre","Héroïque","Histoire","Horreur","Humour","Josei","Journalisme","Kawaii","Kodomo","Nekketsu","Pantso shoto","Philosophie","Photographie",
   "Policier","Religion","Romantique","Satirique","SF","Seinen","Shojo","Shonen","Sociologie","Spatial","Sport","Survie","Thriller","Voyage","Western","Yaoi","Yuri"];
   
