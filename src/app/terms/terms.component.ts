6c1549e8dd258205e4e15f58a4db8f295c41b6e1
adamdrira <59208754+adamdrira@users.noreply.github.com>
2021-04-01 06:32:57 +0200

    Add files via upload

--- src/app/terms/terms.component.ts
+++ src/app/terms/terms.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-terms',
@@ -11,45 +10,24 @@ export class TermsComponent implements OnInit {
 
   constructor(
     public route: ActivatedRoute,
-    private title: Title,
-    private meta: Meta,
+    private router: Router,
+    ) {
 
-    ) { }
+      this.router.routeReuseStrategy.shouldReuseRoute = function() {
+        return false;
+      };
+     }
 
-  ngOnInit(): void {
+    article_number:number;
+    ngOnInit(): void {
 
-    this.article_number = parseInt(this.route.snapshot.paramMap.get('article_number'));
-    if( ! (this.article_number>=0 && this.article_number<6) ) {
-      this.article_number = 1;
+      this.article_number = parseInt(this.route.snapshot.paramMap.get('article_number'));
+      if( ! (this.article_number>=0 && this.article_number<6) ) {
+        this.article_number = 1;
+      }
     }
-    
-    if(this.article_number==1) {
-      this.title.setTitle('Mentions légales');
-      this.meta.updateTag({ name: 'description', content: "Lire les mentions légales du réseau LinkArts." });  
-    }
-    else if(this.article_number==2) {
-      this.title.setTitle('Conditions d\'utilisation de LinkArts');
-      this.meta.updateTag({ name: 'description', content: "Lire les conditions d'utilisation du réseau LinkArts." });  
-    }
-    else if(this.article_number==3) {
-      this.title.setTitle('Politique d\'utilisation des cookies de LinkArts');
-      this.meta.updateTag({ name: 'description', content: "Lire la politique d'utilisation des cookies du réseau LinkArts." });  
-    }
-    else if(this.article_number==4) {
-      this.title.setTitle('Règles de confidentialité et de sécurité');
-      this.meta.updateTag({ name: 'description', content: "Lire les règles de confidentialité et de sécurité du réseau LinkArts." });  
-    }
-    else if(this.article_number==5) {
-      this.title.setTitle('Politique d\'utilisation des données');
-      this.meta.updateTag({ name: 'description', content: "Lire la politique d'utilisation des données du réseau LinkArts" });  
-    }
-    else {
-      this.title.setTitle('LinkArts – BD, Dessins et Ecrits');
-      this.meta.updateTag({ name: 'description', content: "Bienvenue sur LinkArts, le site web dédié aux artistes et professionnels du monde de l'édition.  Le site répond avant tout au besoin de collaboration de promotion et de rémunération des artistes et professionnels de l'édition." });
-    }
-  }
 
-  article_number:number;
+
 
 
 }
