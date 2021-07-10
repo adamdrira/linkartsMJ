a05703699a4cc0dd644d5323fc0d6b02c8dc99fa
adamdrira <59208754+adamdrira@users.noreply.github.com>
2021-07-11 01:33:05 +0200

    Add files via upload

--- src/app/services/profile_edition.service.ts
+++ src/app/services/profile_edition.service.ts
@@ -147,7 +147,7 @@ export class Profile_Edition_Service {
   }
 
   retrieve_number_of_contents_by_pseudo(pseudo){
-    return this.httpClient.post('routes/retrieve_number_of_contents_by_pseudo',{pseudo:pseudo},{withCredentials:true} ).pipe(map((information)=>{
+    return this.httpClient.get(`routes/retrieve_number_of_contents_by_pseudo/${pseudo}`,{withCredentials:true} ).pipe(map((information)=>{
       return information;
     }));
   }
