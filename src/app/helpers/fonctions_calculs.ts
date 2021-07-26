3c37a79b7952b512d71f605e833905f0694829b7
adamdrira <59208754+adamdrira@users.noreply.github.com>
2021-07-26 04:21:33 +0200

    Add files via upload

--- src/app/helpers/fonctions_calculs.ts
+++ src/app/helpers/fonctions_calculs.ts
@@ -5,23 +5,11 @@ export function number_in_k_or_m(n: number){
     if(n<1000) {
         return String(n);
     }
-    else if(n<10000) {
+    else if(n<100000) {
         return  (n/1000).toFixed(1) + "k";
     }
-    else if(n<100000) {
-        return (n/10000).toFixed(1) + "k";
-    }
-    else if(n<1000000) {
-        return ( n/100000 ).toFixed(1) + "k" ;
-    }
-    else if(n<10000000) {
-        return ( n/1000000 ).toFixed(1) + "m" ;
-    }
-    else if(n<100000000) {
-        return ( n/10000000 ).toFixed(1) + "m" ;
-    }
     else if(n<1000000000) {
-        return ( n/100000000 ).toFixed(1) + "m" ;
+        return ( n/1000000 ).toFixed(1) + "m" ;
     }
 
 }
