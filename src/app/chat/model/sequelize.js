0d5df14092c0acd33b5875b1f1f42035bfa01d3a
adamdrira <59208754+adamdrira@users.noreply.github.com>
2021-09-11 23:23:24 +0200

    Add files via upload

---
+++ src/app/chat/model/sequelize.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+const tables = require('./tables');
+
+
+const sequelize = new Sequelize('linkarts', 'postgres', 'test', {
+    host : 'localhost',
+    dialect: 'postgres'
+});
+/*const sequelize = new Sequelize('linkarts', 'adamdrira', 'E273adamZ9Qvps', {
+  host : 'localhost',
+  dialect: 'postgres'
+});*/
+
+
+const list_of_messages= tables.list_of_messages(sequelize, Sequelize).list_of_messages;
+const list_of_chat_friends= tables.list_of_messages(sequelize, Sequelize).list_of_chat_friends;
+const list_of_chat_spams= tables.list_of_messages(sequelize, Sequelize).list_of_chat_spams;
+const list_of_chat_search= tables.list_of_messages(sequelize, Sequelize).list_of_chat_search;
+const list_of_chat_sections= tables.list_of_messages(sequelize, Sequelize).list_of_chat_sections;
+const list_of_chat_groups= tables.list_of_messages(sequelize, Sequelize).list_of_chat_groups;
+const list_of_chat_groups_reactions= tables.list_of_messages(sequelize, Sequelize).list_of_chat_groups_reactions;
+const list_of_chat_emails= tables.list_of_messages(sequelize, Sequelize).list_of_chat_emails;
+const list_of_chat_folders= tables.list_of_messages(sequelize, Sequelize).list_of_chat_folders;
+const list_of_chat_contracts= tables.list_of_messages(sequelize, Sequelize).list_of_chat_contracts;
+
+//Pass { force: true } as option if you want to force delete and recreate.
+sequelize.sync() 
+  .then(() => {
+    console.log(`Database & tables created for list_of_messages!`);
+});
+
+
+module.exports = {
+  list_of_chat_friends,
+  list_of_chat_groups,
+  list_of_chat_groups_reactions,
+  list_of_chat_spams,
+  list_of_messages,
+  list_of_chat_emails,
+  list_of_chat_search,
+  list_of_chat_sections,
+  list_of_chat_folders,
+  list_of_chat_contracts,
+  sequelize
+}
+
